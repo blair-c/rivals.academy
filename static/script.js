a01c5c8e8a3007f277a6f64ce234100e5ab6f634
Blair C <bscardiel@gmail.com>
2023-01-12 06:39:46 -0500

    Add copy heading link feedback tooltip

--- static/script.js
+++ static/script.js
@@ -6,3 +6,15 @@ function toggleHambMenu() {
     `${!(hamb.getAttribute('aria-expanded') === 'true')}`
   );
 }
+
+async function copyHeadingLink(section) {
+  // Copy
+  navigator.clipboard.writeText(window.location.href + "#" + section);
+  // Display tooltip
+  var heading = document.getElementById(section);
+  if (!heading.classList.contains("data-copy-success")) {
+    heading.classList.toggle("data-copy-success");
+    await new Promise(r => setTimeout(r, 2000));
+    heading.classList.toggle("data-copy-success");
+  }
+}
