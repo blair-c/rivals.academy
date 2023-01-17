be60fa3d023a78a0c06e3869e0fb34a67f664616
blair-c <blair-c@users.noreply.github.com>
2023-01-17 04:49:38 +0000

    deploy: d9d024a22e33eeb2b2d11c9f1c183e226206b56d

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function toggleHambMenu() {
-  var hamb = document.querySelector(".hamb-btn")
-  hamb.classList.toggle("data-open")
+  var hamb = document.querySelector('.hamb-btn')
+  hamb.classList.toggle('data-open')
   hamb.setAttribute(
     'aria-expanded', 
     `${!(hamb.getAttribute('aria-expanded') === 'true')}`
   );
+  document.body.classList.toggle('data-disable-scroll-mobile');
 }
 
 async function copyHeadingLink(section) {
   // Copy
   var baseUrl = window.location.href.split('#')[0];
-  navigator.clipboard.writeText(baseUrl + "#" + section);
+  navigator.clipboard.writeText(baseUrl + '#' + section);
   // Display tooltip
   var heading = document.getElementById(section);
-  if (!heading.classList.contains("data-copy-success")) {
-    heading.classList.toggle("data-copy-success");
+  if (!heading.classList.contains('data-copy-success')) {
+    heading.classList.toggle('data-copy-success');
     await new Promise(r => setTimeout(r, 2000));
-    heading.classList.toggle("data-copy-success");
+    heading.classList.toggle('data-copy-success');
   }
 }
