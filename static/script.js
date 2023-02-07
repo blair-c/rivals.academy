5487f43f5b5d998260195af12991dba0bf85f366
Blair C <bscardiel@gmail.com>
2023-02-07 17:19:40 -0500

    switch to styling hamb based on aria-expanded

--- static/script.js
+++ static/script.js
@@ -1,6 +1,5 @@
 function toggleHambMenu() {
   var hamb = document.querySelector('.hamb-btn')
-  hamb.classList.toggle('data-open')
   hamb.setAttribute(
     'aria-expanded', 
     `${!(hamb.getAttribute('aria-expanded') === 'true')}`
