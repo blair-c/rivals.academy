684cec9a1129782d39bd3363f1aa6ce426a03792
blair-c <blair-c@users.noreply.github.com>
2023-03-03 02:16:24 +0000

    deploy: 392bbd8df91d4d52d108a0b34b76398dd652739e

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ if (toc) {
 	// Factor of screen size that the element must cross
 	// before it's considered visible
 	var TOP_MARGIN = -0.1,
-		  BOTTOM_MARGIN = 0.2;
+      BOTTOM_MARGIN = 0.2;
 
   window.addEventListener( 'scroll', sync, false );
   sync();
